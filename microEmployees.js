b886b7864b008f4d310f370db4f25493c10d69fe
bruto8000 <54044706+bruto8000@users.noreply.github.com>
2021-05-10 01:57:43 +0300

    Added query

--- microEmployees.js
+++ microEmployees.js
@@ -2,15 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const cors = require('cors')
+
+
 
 const mysql = require("mysql");
 
-let db_config = {
-  host: "192.168.0.104",
-  user: "root",
-  password: "bruto",
-  database: "bruto",
-};
+let db_config = require('./connection.js');
 let searchColumns = [
   { name: "winlogin", operator: "=" },
   { name: "personnelNumber", operator: "=" },
@@ -25,6 +23,8 @@ function handleDisconnect() {
   connection.connect(function (err) {
     if (err) {
       setTimeout(handleDisconnect, 2000);
+    }else{
+      console.log('connected to database')
     }
   });
 
@@ -40,8 +40,8 @@ handleDisconnect();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors())
 
-// app.use((req, res, next) => {});
 app.all("*", (req, res, next) => {
   console.log(`request URL : ${req.url} //\\\\ METHOD IS ${req.method}`);
 
@@ -49,40 +49,51 @@ app.all("*", (req, res, next) => {
 });
 
 function validateReq(req, res, next) {
+  if(!Object.keys(req.body).length && Object.keys(req.query).length){
+    req.body = req.query;
+  }
+
   if (!searchColumns.map((column) => column.name).includes(req.body.searchBy)) {
     res.status(400).end("BAD REQUEST");
-  } else {
-      if(!req.body.limit || ( !isNaN(Number(req.body.limit)) && (req.body.limit >20 || req.body.limit <1))){
-          req.body.limit = 20
-      }
-    next();
+    return;
   }
+
+  next();
 }
 function modifyOperatorAndParameter(req, res, next) {
+  if (
+    !req.body.limit ||
+    isNaN(Number(req.body.limit)) ||
+    (!isNaN(Number(req.body.limit)) &&
+      (req.body.limit > 20 || req.body.limit < 1))
+  ) {
+    req.body.limit = 20;
+  }
+
   req.body.operator = searchColumns.find(
     (column) => column.name == req.body.searchBy
   ).operator;
+
+  
+
   if (req.body.operator == "LIKE") {
     req.body.parameter = `%${req.body.parameter}%`;
   }
+
   next();
 }
 app.get("/employees", validateReq, modifyOperatorAndParameter, (req, res) => {
-    let sql =    `SELECT * FROM EMPLOYEES WHERE ${req.body.searchBy} ${req.body.operator} '${req.body.parameter}' LIMIT ${req.body.limit}`;
-    console.log(sql)
-  executeQuery(
- sql
-  )
+  let sql = `SELECT * FROM EMPLOYEES WHERE ${req.body.searchBy} ${req.body.operator} '${req.body.parameter}' LIMIT ${req.body.limit}`;
+  console.log(sql);
+  executeQuery(sql)
     .then((results) => {
       res.end(JSON.stringify(results));
     })
     .catch((err) => {
-        console.log(err)
-      res.status(500).end("500");
+      console.log(err);
+      res.status(500).end(JSON.stringify(err));
     });
 });
-
-
 
 app.all("*", (req, res) => {
   res.status(404).end("NOT FOUND");
